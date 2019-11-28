17c89ffe4017247e8da1dd6680894a938fa2e901
Sri Hari Duvvuri <55041350+duvvurisrihari@users.noreply.github.com>
2019-11-28 00:51:23 -0800

    login changed

--- Frontend/src/components/Login/Login.js
+++ Frontend/src/components/Login/Login.js
@@ -50,6 +50,7 @@ export default class Login extends Component {
     axios.post(`${ROOT_URL}/users/login`, data)
     .then(res => {
       console.log(res.status +  "Resulyt bkwsde");
+      console.log('hi');
      // var resultData = res.data[0];
       if(res.status === 200){
      
