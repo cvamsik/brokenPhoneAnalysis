18b13f6f25b0593b735c2b4609a07acf3c4768ba
cvamsik <55032250+cvamsik@users.noreply.github.com>
2019-12-02 21:23:54 -0800

    faq page updated

--- Frontend/src/components/FAQ/faq.js
+++ Frontend/src/components/FAQ/faq.js
@@ -4,11 +4,12 @@ import './faq.css';
 class faq extends Component{
     render(){
         return(
+          <div className="faqcontent">
  <div id="accordion" class="accordian">
   <div class="card">
     <div class="card-header" id="headingOne">
       <h5 class="mb-0">
-        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <button class="btn btn-link text-white" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
           Our pricing determination?
         </button>
       </h5>
@@ -29,7 +30,7 @@ class faq extends Component{
   <div class="card">
     <div class="card-header" id="headingTwo">
       <h5 class="mb-0">
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <button class="btn btn-link text-white collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
             What happens to my device once I’ve ended my transaction
         </button>
       </h5>
@@ -43,7 +44,7 @@ class faq extends Component{
   <div class="card">
     <div class="card-header" id="headingThree">
       <h5 class="mb-0">
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        <button class="btn btn-link collapsed text-white" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
         Can I get my device back after I've sold it?
         </button>
       </h5>
@@ -56,7 +57,7 @@ class faq extends Component{
   <div class="card">
     <div class="card-header" id="headingThree">
       <h5 class="mb-0">
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        <button class="btn btn-link text-white collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
         Do you accept accessories, like chargers?
         </button>
       </h5>
@@ -67,7 +68,7 @@ class faq extends Component{
     </div>
   </div>
 </div>
-
+</div>
         );
     }
 }
